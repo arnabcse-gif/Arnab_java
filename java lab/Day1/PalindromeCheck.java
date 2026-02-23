c90d6df8bc44b2be6b8a79a51fd167bd0cbc1ca5
arnabcse-gif <arnabchakraborty922@gmail.com>
2026-02-24 01:00:00 +0530

    Updated Day1 folder

---
+++ java lab/Day1/PalindromeCheck.java
@@ -0,0 +1,18 @@
+class PalindromeCheck {
+    public static void main(String[] args) {
+        int num = 121;
+        int original = num;
+        int reverse = 0;
+
+        while(num != 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num = num / 10;
+        }
+
+        if(original == reverse)
+            System.out.println("The number is Palindrome");
+        else
+            System.out.println("The number is not Palindrome");
+    }
+}
