14ec70a0e09c39fde07a07e64fc1c525edd18a4f
arnabcse-gif <arnabchakraborty922@gmail.com>
2026-02-24 00:50:51 +0530

    Updated Day1 folder

---
+++ java lab/Day1/StudentDetails.java
@@ -0,0 +1,13 @@
+class StudentDetails {
+    public static void main(String[] args) {
+        String name = "Arnab Chakraborty";
+        String rollNo = "24051994";
+        String section = "CSE-16";
+        String branch = "Computer Science and Engineering";
+
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Section: " + section);
+        System.out.println("Branch: " + branch);
+    }
+}
